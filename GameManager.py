a6097b67091af808c3eecf70e66af1ab31aeec57
Tomcio <tomasz.wolak@pl.ibm.com>
2018-01-10 09:16:25 +0100

    "Grid - clone method added "

--- GameManager.py
+++ GameManager.py
@@ -1,34 +1,39 @@
 import Grid
 
-class GameManager(object):
-	def __init__(self):
-		self.grid = Grid.Grid()
-	def main(self):
-		while not self.grid.isOver():
-			while True:
-				self.grid.printGrid()
-				userMove = input("> ")
-				sign = userMove[1]
-				field = int(userMove[0])
-				validation = self.validateInput(sign, field)
-				if validation == "":
-					if self.grid.makeMove(field,sign):
-						break
-					else:
-						print("Move already done")
-				else:
-					print(validation)
-		self.grid.printGrid()
 
-			
-	def validateInput(self, sign, field):
-		strMessage = ""
-		if sign != "X" and sign != "O":
-			strMessage = "Invalid sign"
-		elif field <0 or field> 8:
-			strMessage = "Invalid field index"
-		return strMessage
-def run():
-	manager = GameManager()
-	manager.main()
-run()
+class GameManager(object):
+    def __init__(self):
+        self.grid = Grid.Grid()
+
+    def main(self):
+        while not self.grid.isOver():
+            while True:
+                self.grid.printGrid()
+                userMove = input("> ")
+                sign = userMove[1]
+                field = int(userMove[0])
+                validation = self.validateInput(sign, field)
+                if validation == "":
+                    if self.grid.makeMove(field, sign):
+                        break
+                    else:
+                        print("Move already done")
+                else:
+                    print(validation)
+        self.grid.printGrid()
+
+    def validateInput(self, sign, field):
+        strMessage = ""
+        if sign != "X" and sign != "O":
+            strMessage = "Invalid sign"
+        elif field < 0 or field > 8:
+            strMessage = "Invalid field index"
+        return strMessage
+
+
+def main():
+    manager = GameManager()
+    manager.main()
+
+
+main()
--- Grid.py
+++ Grid.py
@@ -1,81 +1,84 @@
-
-
+from copy import deepcopy
 class Grid(object):
-	def __init__(self):
-		self.gridMap = {x: "." for x in range(9) }
+    def __init__(self):
+        self.gridMap = {x: "." for x in range(9)}
 
-	def printGrid(self):
-		for i in range(len(self.gridMap)):
-				if (i +1) % 3   == 0:
-					print(self.gridMap[i], end= "\n")
-				else:
-					print(self.gridMap[i], end= " ")
-			
-	def getAvailableMoves(self):
-		moves = list()
-		for i in range(len(self.gridMap)):
-			if self.gridMap[i] == ".":
-				moves.append(i)
-		return moves
+    def printGrid(self):
+        for i in range(len(self.gridMap)):
+            if (i + 1) % 3 == 0:
+                print(self.gridMap[i], end="\n")
+            else:
+                print(self.gridMap[i], end=" ")
 
-	def makeMove(self, move, sign):
-		if self.gridMap[move] == ".":
-			self.gridMap[move] = sign
-			return True
-		else:
-			return False
+    def clone(self):
+        gridCopy = Grid()
+        gridCopy.gridMap = deepcopy(self.gridMap)
+        return gridCopy
 
-	def isOver(self):
-		if len(self.getAvailableMoves()) == 0:
-			return True
-		elif self.checkHorizontally():
-			#print("horizontally")
-			return True
-		elif self.checkVertically():
-			#print("vertically")
-			return True
-		elif self.checkDiagonally():
-			#print("diagonally")
-			return True
-		else:
-			return False
+    def getAvailableMoves(self):
+        moves = list()
+        for i in range(len(self.gridMap)):
+            if self.gridMap[i] == ".":
+                moves.append(i)
+        return moves
 
-	def checkHorizontally(self):
-		for i in (0,3,6):
-			firstRow = set()
-			for j in range(3):
-				first = self.gridMap[i+j]
-				firstRow.add(first)
-				#print(f"lenght of the set is: {len(firstRow)}")
-				#print("row contains: " , firstRow)
-			if len(firstRow) == 1 and first != ".":
-				return True
-		return False
-	
-	def checkDiagonally(self):
-		firstLine = set()
-		for i in (0,4,8):
-			first = self.gridMap[i]
-			firstLine.add(first)
-		if len(firstLine) == 1 and first != ".":
-			return True
-		firstLine = set()
-		for i in (2,4,6):
-			first = self.gridMap[i]
-			firstLine.add(first)
-		if len(firstLine) == 1 and first != ".":
-			return True
-		return False
+    def makeMove(self, move, sign):
+        if self.gridMap[move] == ".":
+            self.gridMap[move] = sign
+            return True
+        else:
+            return False
 
-	def checkVertically(self):
-		for i in (0,1,2):
-			firstColumn = set()
-			for j in (0,3,6):
-				first = self.gridMap[i+j]
-				firstColumn.add(first)
-				#print(f"lenght of the set is: {len(firstColumn)}")
-				#print("column contains: " , firstColumn)
-			if len(firstColumn) == 1 and first != ".":
-				return True
-		return False
-		
+    def isOver(self):
+        if len(self.getAvailableMoves()) == 0:
+            return True
+        elif self.checkHorizontally():
+            # print("horizontally")
+            return True
+        elif self.checkVertically():
+            # print("vertically")
+            return True
+        elif self.checkDiagonally():
+            # print("diagonally")
+            return True
+        else:
+            return False
+
+    def checkHorizontally(self):
+        for i in (0, 3, 6):
+            firstRow = set()
+            for j in range(3):
+                first = self.gridMap[i + j]
+                firstRow.add(first)
+            # print(f"lenght of the set is: {len(firstRow)}")
+            # print("row contains: " , firstRow)
+            if len(firstRow) == 1 and first != ".":
+                return True
+        return False
+
+    def checkDiagonally(self):
+        firstLine = set()
+        for i in (0, 4, 8):
+            first = self.gridMap[i]
+            firstLine.add(first)
+        if len(firstLine) == 1 and first != ".":
+            return True
+        firstLine = set()
+        for i in (2, 4, 6):
+            first = self.gridMap[i]
+            firstLine.add(first)
+        if len(firstLine) == 1 and first != ".":
+            return True
+        return False
+
+    def checkVertically(self):
+        for i in (0, 1, 2):
+            firstColumn = set()
+            for j in (0, 3, 6):
+                first = self.gridMap[i + j]
+                firstColumn.add(first)
+            # print(f"lenght of the set is: {len(firstColumn)}")
+            # print("column contains: " , firstColumn)
+            if len(firstColumn) == 1 and first != ".":
+                return True
+        return False
